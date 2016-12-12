b90cbf6fd181550fda64e143eae798abcc2b18c8
“Matt <matthyman03@gmail.com>
2016-12-11 17:09:21 -0700

    Checkpoint-5 final

--- app/scripts/services/BlocChatCookies.js
+++ app/scripts/services/BlocChatCookies.js
@@ -1,35 +1,38 @@
 (function() {	
-
 	function BlocChatCookies($cookies, $uibModal) {
 
 		var template = "<h1 class='set-user-h1'>Set Username</h1>" +
-		"<input class='set-username' placeholder='Enter your username here' ng-model='setNewUsername.name' required type='text'>" +
-		"<button type='button' class='userBtn btn-primary' ng-click='userCtrl.setUserName()' data-dismiss='modal' >Set Username</button>";
+		"<input class='set-username' ng-model='newUsername' required type='text'>" +
+		"<button type='button' class='userBtn btn-primary' ng-click='create()'>Set Username</button>";
 
-		var currentUser = $cookies.get('BlocChatCurrentUser');
+		if (!$cookies.get('blocChatCurrentUser') || $cookies.get('blocChatCurrentUser') === '') {
+			var modalInstance = $uibModal.open({
+				template: template,
+				controller: function($scope, $uibModalInstance) {
+					$scope.newUsername = "User " + Math.floor(Math.random() * (100 - 1)) + 1;
+					$scope.create = function() {
+						if ($scope.newUsername == undefined || $scope.newUsername === "") {
+							alert("Username cannot be blank!");
+						} else {
+							$uibModalInstance.close($scope.newUsername);
+							console.log("Username: " + $scope.newUsername);
+						}
 
-		if (!currentUser || currentUser === '') {
-			$uibModal.open({
-				ariaLabelledBy: 'modal-title',
-		        ariaDescribedBy: 'modal-body',
-		        template: template,
-		        controller: 'SetUsernameInstanceModal',
-		        controllerAs: 'userCtrl'
-      		})
+						var username = $scope.newUsername;
+					};
+				},
+				size: 'md'
+			});
+			
+			modalInstance.result.then(function(data) {
+				$cookies.put('blocChatCurrentUser', data);
+			});
 		}
+
 	}
 
 	angular
 		.module('blocChat')
 		.run(['$cookies', '$uibModal', BlocChatCookies])
-		.controller('SetUsernameInstanceModal', ['$uibModal', '$cookies', function($uibModalInstance, $cookies) {
 
-
-    		this.setUserName = function () {
-    			var test = "testing testing";
-    			console.log(test);
-    			
-    			
-		    }
-		}])
 })();
